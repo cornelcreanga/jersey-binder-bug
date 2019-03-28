414c9bb4d3ab9476f9194d5b56f853d2c628a9c3
creanga <creanga@adobe.com>
2019-03-28 13:05:22 +0200

    fixed by changing the import

--- src/main/java/com/example/jerseybinderbug/TestContextBinder.java
+++ src/main/java/com/example/jerseybinderbug/TestContextBinder.java
@@ -1,7 +1,8 @@
 package com.example.jerseybinderbug;
 
 import javax.inject.Singleton;
-import org.glassfish.hk2.utilities.binding.AbstractBinder;
+//import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.glassfish.jersey.internal.inject.AbstractBinder;
 
 class TestContextBinder extends AbstractBinder {
 
